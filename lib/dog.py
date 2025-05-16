aef219e6b1563e5a6ecbc3852be029f65def562c
gerrad <gerrad.meitamei@student.moringaschool.com>
2025-05-16 14:21:30 +0300

    initial commit

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,22 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    APPROVED_BREEDS = ["Pug", "Corgi", "Beagle", "Dalmatian"]
+
+    def __init__(self, name=None, breed="Mutt"):
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name
+
+        if not isinstance(breed, str) or breed not in self.APPROVED_BREEDS + ["Mutt"]:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self.breed = breed
+
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
+
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,21 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    APPROVED_JOBS = ["Sales", "Engineer", "Manager", "ITC"]
+
+    def __init__(self, name=None, job=None):
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name.title()
+
+        if job is not None and job not in self.APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self.job = job
+
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
